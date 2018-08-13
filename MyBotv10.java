769e531f8a4588c0674fd9a1f95205109cef6105
lgeng12 <larry.geng12@gmail.com>
2018-08-12 22:55:24 -0400

    added comments to v10 bot

--- MyBotv10.java
+++ MyBotv10.java
@@ -9,26 +9,28 @@ public class MyBotv10 {
     private static GameMap gameMap = null;
     public static void main(final String[] args) {
         final Networking networking = new Networking();
-        gameMap = networking.initialize("Defender 2");
+        gameMap = networking.initialize("Defender 2");  //temp name to test undocking
 
         final ArrayList<Move> moveList = new ArrayList<>();
         for (;;) {
+            //reset for this turn
             moveList.clear();
             networking.updateMap(gameMap);
-            Map<Integer, Integer> targetedPlanets = new HashMap<Integer, Integer>();
-            Map<Integer, Integer> targetedShips = new HashMap<Integer, Integer>();
+            Map<Integer, Integer> targetedPlanets = new HashMap<Integer, Integer>();  //maps all planets to number of ships moving to or docking there
+            Map<Integer, Integer> targetedShips = new HashMap<Integer, Integer>();  //maps enemy ships to number of ships moving there
+            //initialize maps
             for(Planet p: gameMap.getAllPlanets().values())
                 targetedPlanets.put(p.getId(), 0);
             for(Ship s: gameMap.getAllShips())
                 if(belongsToEnemy(s))
                     targetedShips.put(s.getId(), 0);
-
+            //step through each ship
             for(final Ship ship: gameMap.getMyPlayer().getShips().values()) {
                 Move command = null;
-                Ship nearestAvailFriendly = null;
+                Ship nearestAvailFriendly = null;  //currently unused
                 Ship nearestDockedFriendly = null;
                 Map<Double, Entity> entities = gameMap.nearbyEntitiesByDistance(ship);
-                /*
+                /*  //in-progress code for undocking to defend
                 if(ship.getDockingStatus() != Ship.DockingStatus.Undocked) {
                     Ship nearestEnemy = null;
                     double distToEnemy = 0;
@@ -49,19 +51,20 @@ public class MyBotv10 {
                         targetedPlanets.put(ship.getDockedPlanet(), targetedPlanets.get(ship.getDockedPlanet())+1);
                     continue;
                 }*/
+                //account for being docked on a planet
                 if(ship.getDockingStatus() != Ship.DockingStatus.Undocked){
                     targetedPlanets.put(ship.getDockedPlanet(), targetedPlanets.get(ship.getDockedPlanet())+1);
                     continue;
                 }
-
+                //target something
                 for(double distance: entities.keySet()){
                     Entity entity = entities.get(distance);
-                    if(entity instanceof Planet && !belongsToEnemy(entity)){
-                        if(targetedPlanets.get(entity.getId()) >= ((Planet)entity).getDockingSpots())
+                    if(entity instanceof Planet && !belongsToEnemy(entity)){  //planet either neutral or friendly
+                        if(targetedPlanets.get(entity.getId()) >= ((Planet)entity).getDockingSpots())  //check to see if planet is fully targeted
                             continue;
-                        if(ship.canDock((Planet)entity))
+                        if(ship.canDock((Planet)entity))  //dock if possible
                             command = new DockMove(ship, (Planet)entity);
-                        else if( ((Planet)entity).getDockedShips().size() < ((Planet)entity).getDockingSpots() )
+                        else if( ((Planet)entity).getDockedShips().size() < ((Planet)entity).getDockingSpots() )  //move to dock if possible
                             command = Navigation.navigateShipToDock(gameMap, ship, entity, Constants.MAX_SPEED);
                         targetedPlanets.put(entity.getId(), targetedPlanets.get(entity.getId()) + 1);
                         break;
@@ -73,7 +76,7 @@ public class MyBotv10 {
                             else if( ((Ship) entity).getDockingStatus() != Ship.DockingStatus.Undocked && nearestDockedFriendly == null)
                                 nearestDockedFriendly = (Ship) entity;
                         }
-                        else if(belongsToEnemy(entity) && targetedShips.get(entity.getId()) < 4){
+                        else if(belongsToEnemy(entity) && targetedShips.get(entity.getId()) < 4){  //attack enemy ship as long as < 4 other friendlies are attacking it
                             command = moveToEntity(gameMap, ship, entity);
                             targetedShips.put(entity.getId(), targetedShips.get(entity.getId())+1);
                             break;
